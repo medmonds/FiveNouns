9cb9e0705106b05a86dfd4a7e5d7c1580218710c
Matthew Edmonds <matthewedmonds@me.com>
2013-04-25 23:34:59 -0700

    started work on the Players view

--- Five Nouns/FNAddPlayersVC.m
+++ Five Nouns/FNAddPlayersVC.m
@@ -14,7 +14,8 @@
 #import "FNPlayer.h"
 
 @interface FNAddPlayersVC ()
-
+@property BOOL addPlayerIsVisible;
+@property (nonatomic, strong) FNPlayer *currentPlayer;
 @end
 
 @implementation FNAddPlayersVC
@@ -28,26 +29,35 @@
     [self.tableView reloadData];
 }
 
+- (void)toggleAddPlayer
+{
+    if (!self.addPlayerIsVisible) {
+        self.addPlayerIsVisible = YES;
+        if (!self.currentPlayer) {
+            self.currentPlayer = [[FNPlayer alloc] init];
+        }
+    } else {
+        self.addPlayerIsVisible = NO;
+    }
+}
+
 #pragma mark - Table view delegate
 
 - (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if (indexPath.section == 1) {
-        return NO;
-    } else {
-        return YES;
+    if (indexPath.section == 0) {
+        if (indexPath.row == 0) {
+            [self toggleAddPlayer];
+        } else {
+            // make the textfield (if cell has a textfield) the first responder
+        }
     }
+    return NO;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if (indexPath.section == 0) {
-        if (indexPath.row == 0) {
-            [self performSegueWithIdentifier:@"createPlayer" sender:self];
-        } else {
-            [self performSegueWithIdentifier:@"teamsOverview" sender:self];
-        }
-    }
+    // do nothing here cells should never be selected
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -63,13 +73,23 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 2;
+    if ([self.brain.allPlayers count] > 0) {
+        return 2;
+    }
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     if (section == 0) {
-        return 2;
+        if (self.addPlayerIsVisible) {
+            // add player cell + name + save cell + (minimum of 3 nouns)
+            // return (3 + MAX([self.currentPlayer.nouns count], 3));
+            // a fixed number of nouns is sooooo much easier
+            return 8;
+        } else {
+            return 1;
+        }
     } else if (section == 1) {
         return [self.brain.allPlayers count];
     } else {
@@ -79,13 +99,21 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    // need to make and use the right kind of cells
     if (indexPath.section == 0) {
         NSString *CellIdentifier = @"navigationCell";
         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
         if (indexPath.row == 0) {
             cell.textLabel.text = @"Add Player";
+        } else if (indexPath.row == 1) {
+            cell.textLabel.text = @"Name";
+            // set the name in the textfield
+        } else if (indexPath.row != [self.tableView numberOfRowsInSection:indexPath.section] - 1) {
+            // it is a noun cell assuming a fixed number of nouns
+            cell.textLabel.text = @"Noun";
+            // set the nouns in the textfields
         } else {
-            cell.textLabel.text = @"Next";
+            // do nothing the button will already say "Save"
         }
         return cell;
     } else {
