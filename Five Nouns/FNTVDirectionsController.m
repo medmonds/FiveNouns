d09beb5304305262f5cef1e3c4fc40bc7ccfab7f
Matthew Edmonds <matthewedmonds@me.com>
2013-06-28 18:00:20 -0700

    forking to make the FNTV subclassed not with a delegate

---
+++ Five Nouns/FNTVDirectionsController.m
@@ -0,0 +1,13 @@
+//
+//  FNTVDirectionsController.m
+//  Five Nouns
+//
+//  Created by Matthew Edmonds on 6/28/13.
+//  Copyright (c) 2013 Matthew Edmonds. All rights reserved.
+//
+
+#import "FNTVDirectionsController.h"
+
+@implementation FNTVDirectionsController
+
+@end
